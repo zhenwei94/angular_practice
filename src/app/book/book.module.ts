5e4ca03600c16039d8f832a5fa0e66634cdc1552
Evan <evancjx@gmail.com>
2020-11-21 09:55:54 +0800

    add book component & route book url

--- src/app/book/book.module.ts
+++ src/app/book/book.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { BookRoutingModule } from './book-routing.module';
+import { BookComponent } from './book.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [BookComponent],
   imports: [
     CommonModule,
     BookRoutingModule
